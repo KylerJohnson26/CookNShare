4fd8731888ef5f96f1fb4a3bcc70c01d52fb0939
kljohnson2 <kylerljohnson@icloud.com>
2018-08-25 18:32:05 -0400

    changing User property of Recipe class to conventional pascal casing, adding userId property to establish relationship

--- CookNShareWebApi/Models/Recipe.cs
+++ CookNShareWebApi/Models/Recipe.cs
@@ -6,7 +6,8 @@ namespace CookNShareWebApi.Models
     public class Recipe
     {
         public int Id { get; set; }
-        public User user { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime DateCreated { get; set; }
